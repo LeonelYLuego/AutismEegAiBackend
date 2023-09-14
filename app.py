d1794a65330e8d21ee7551944c48c33d8f209458
Leonel Fernández <leonel-leonel-1@hotmail.com>
2023-09-13 22:00:11 -0600

    Docker added

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://root:root@localhost:5432/autism"
+db = SQLAlchemy(app)
+
+# Create a Flask app context
+app.app_context().push()
+
+# Create the database tables within the app context
+db.create_all()
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run()
