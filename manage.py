d5cf3fc856d1263eb44d20fbb9475f6899db6d02
Gerardo Gomez <olikase24@gmail.com>
2023-09-15 16:26:35 -0600

    Change the app.py for manage.py

---
+++ manage.py
@@ -0,0 +1,11 @@
+import os
+
+from app.main import create_app
+from app.main.model import user
+
+app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+
+app.app_context().push()
+
+if __name__ == '__main__':
+    app.run()
